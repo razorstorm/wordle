6675154bbf43bc184cf31f1afe6e758cdf1e67d1
Jason Jiang <petrifiednightmares@gmail.com>
2022-01-09 17:49:03 -0800

    Init

--- src/js/game.ts
+++ src/js/game.ts
@@ -10,46 +10,33 @@ declare global {
 }
 
 jQuery(function () {
-  let playerData = localStorage.getItem('player');
-  let player: Player = new Player(JSON.parse(playerData));
-  let previousTime: Date = null;
-  let panels: Panels = new Panels();
-  const framesPerSecond: number = 30.0;
-  const secondsPerSave: number = 5;
-  const gameStorage = window.localStorage;
-  const log = $("#log");
+  let currRow = $("#guesses>.row:first-of-type");
+  let currGuess = "";
+  console.log(currRow);
 
-  const runFrame = () => {
-    const currTime = new Date();
-    if (previousTime === null) {
-      previousTime = currTime;
-      return;
+  const doc = $(document);
+  const makeGuess = (letter: string) => {
+    currGuess += letter;
+    const currBox = currRow.children(".empty").first();
+    console.log(currBox);
+    currBox.text(letter);
+    currBox.removeClass("empty");
+  }
+
+  const isLetter = (text: string): boolean => {
+    if(text.length !== 1) {
+      return false;
     }
-    const timeElapsed: number = currTime.valueOf() - previousTime.valueOf();
-
-    player.runFrame(timeElapsed);
-
-    panels.updateData(player);
-    previousTime = currTime;
+    return (/[a-zA-Z]/).test(text);
   }
 
-  const saveGame = () => {
-    gameStorage.setItem('player', JSON.stringify(player));
-    log.text(`Last saved at ${new Date()}`);
-  }
+  doc.on('keyup', function (e) {
+    const char = e.originalEvent.key.toUpperCase();
+    console.log(char);
+    console.log(isLetter(char));
 
-  window.makeCode = () => {
-    player.makeCode();
-  }
-
-  window.hireDev = () => {
-    player.hireDev();
-  }
-
-  window.upgradeDev = () => {
-    player.upgradeDev();
-  }
-
-  const gameClock = setInterval(runFrame, 1000 / framesPerSecond);
-  const saveClock = setInterval(saveGame, secondsPerSave * 1000);
+    if(isLetter(char)) {
+      makeGuess(char);
+    }
+  });
 });
